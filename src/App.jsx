5944ad80351fbad6a7f88f39dc831d79f37693c9
Klaas Van Mechelen <165936240+klaasvm@users.noreply.github.com>
2025-03-29 11:01:39 +0000

    codespaces-project-template-js

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Klaas Van Mechelen",
+  title: "Web Designer & Software engineer",
+  email: "klaas@example.com",
+  gitHub: "klaasvm",
+  instagram: "klaasvm",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "klaasvm",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/portofolio-image.png";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Steam",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Steam is a digital distribution platform for video games, developed by Valve Corporation. It offers a wide range of games, software, and other content for purchase and download.",
+    url: "https://store.steampowered.com",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Tweakers",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Tweakers is a Dutch technology website that focuses on news, reviews, and discussions about technology, gadgets, and software.",
+    url: "https://tweakers.net",
   },
   {
-    title: "My Resume Site",
+    title: "klaasvm.com",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "This is my own site and space on the web where I can share my work, projects and do whatever I want.",
+    url: "https://www.klaasvm.com",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "GitHub",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Github is in my opinion the best place for every developer. I use it almost for everything relevated with coding.",
+    url: "www.youtube.com/watch?v=pBy1zgt0XPc&t",
   },
 ];
 
